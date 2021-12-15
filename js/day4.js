7c579e2b2a317dc72691cc9ce2db1a39db2325f2
ilham <ibouaik@gmail.com>
2021-12-15 20:19:11 +0100

    level 2

---
+++ js/day4.js
@@ -0,0 +1,14 @@
+let num = 100
+if (num < 0 || num > 100 ) {
+    console.log("the grade is out of range 0-100")
+} else if (num <=49)  {
+    console.log(`F`)
+} else if (num<=59) {
+    console.log(`D`)
+} else if (num<=69) {
+    console.log(`C`)
+} else if ( num<=89)  {
+    console.log(`B`)
+} else if ( num<=100 ) {
+    console.log(`A`)
+} 
