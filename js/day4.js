e3d8dbf02b6c9f6f5e66fb8c03a2701152ca61c6
ilham <ibouaik@gmail.com>
2021-12-15 21:46:05 +0100

    level 2 ex2 includes

--- js/day4.js
+++ js/day4.js
@@ -1,11 +1,15 @@
-let month = "July"
+  let month = "October"
+  if ("September, October, November".includes(month)) {
+      console.log(`The season is Autumn.`)
+  } else if ("April, May, March".includes(month)) {
+      console.log(`The season is Spring.`) 
+  } else if ("June, July, August".includes(month)) {
+      console.log(`The season is Summer`)
+  } else if ("December, January, February".includes(month)) {
+      console.log(`The season is Winter`)
+  } else {
+      console.log("the entry is not a month")
+  }
 
-if (month ===  "September" || month ===  " October" || month ===  "November") {
-    console.log(`The season is Autumn.`)
-} else if (month ===  "April" || month ===  "May" || month ===  "March") {
-    console.log(`The season is Spring.`) 
-} else if (month ===  "June" || month ===  "July" || month ===  "August") {
-    console.log(`The season is Summer`)
-}
-   
 
+  
