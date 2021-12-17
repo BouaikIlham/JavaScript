64a2e866948ff55d5ed7abf902dae70aec131bf7
ilham <ibouaik@gmail.com>
2021-12-17 19:53:34 +0100

    Level  2

--- js/day6.js
+++ js/day6.js
@@ -28,5 +28,40 @@ console.log(perimeterOfRectangle(2,2))
       return  (Celcius * 9/5) + 32
     }
     console.log(convertCelciusToFahrenheit(2))
+/////////////////////////////////
+const solveLinEquation = (a, b) =>  {
+  if ( a === 0 ) {
+    return 'a must not be 0'
+  }
+  return -b/a
+}
+console.log(solveLinEquation(2, 5))
 
 
+const  reverseArray = (arr) => {
+  // return arr.reverse() 
+
+  let newArr = []
+  while(arr.length > 0) {
+    newArr.push(arr.pop())
+  }
+
+  return newArr
+}
+console.log(reverseArray([1, 2, 3, 4, 5]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
