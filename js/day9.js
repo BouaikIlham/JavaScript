838767d1e2b84ce9a731bf0d70e8368f88af47b4
ilham <ibouaik@gmail.com>
2021-12-21 19:20:24 +0100

    Dom

---
+++ js/day9.js
@@ -0,0 +1,57 @@
+const articles = [
+    {
+        heading: "hello world",
+        body: "gefoihef zefoihzefijzfe fzeoibzefihfze fzeoihzfepihzef zfeihzefhzef"
+    },
+    {
+        heading: "hello world",
+        body: "gefoihef zefoihzefijzfe fzeoibzefihfze fzeoihzfepihzef zfeihzefhzef"
+    },
+    {
+        heading: "hello world",
+        body: "gefoihef zefoihzefijzfe fzeoibzefihfze fzeoihzfepihzef zfeihzefhzef"
+    },
+    {
+        heading: "hello world",
+        body: "gefoihef zefoihzefijzfe fzeoibzefihfze fzeoihzfepihzef zfeihzefhzef"
+    },
+    {
+        heading: "hello world",
+        body: "gefoihef zefoihzefijzfe fzeoibzefihfze fzeoihzfepihzef zfeihzefhzef"
+    },
+    {
+        heading: "hello world",
+        body: "gefoihef zefoihzefijzfe fzeoibzefihfze fzeoihzfepihzef zfeihzefhzef"
+    },
+    {
+        heading: "hello world",
+        body: "gefoihef zefoihzefijzfe fzeoibzefihfze fzeoihzfepihzef zfeihzefhzef"
+    },
+    {
+        heading: "hello world",
+        body: "gefoihef zefoihzefijzfe fzeoibzefihfze fzeoihzfepihzef zfeihzefhzef"
+    }
+]
+
+const root = document.getElementById("root")
+
+articles.map((article) => {
+    const heading = document.createElement("h1")
+    const body = document.createElement("div")
+
+    heading.classList.add('test')
+
+    heading.innerHTML = article.heading
+    body.innerHTML = article.body
+
+    root.appendChild(heading)
+    root.appendChild(body)
+})
+
+root.addEventListener('click', (e) => {
+    if (e.target.classList.contains("test")) {
+        e.target.classList.remove('test')
+    } else {
+        e.target.classList.add('test')
+    }
+})
