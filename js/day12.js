c35732edb50dbfc0c49dc3c7b117fbcbabae1fa5
ilham <ibouaik@gmail.com>
2021-12-23 23:05:22 +0100

    addevent

--- js/day12.js
+++ js/day12.js
@@ -14,3 +14,15 @@ button.addEventListener('click', (e) => {
 
     
 })
+//////////////////////////////////////////////////////////////////////
+const mass = document.querySelector('#mass')
+const height = document.querySelector('#height')
+const button = document.querySelector('button')
+const message = document.getElementById("message")
+
+let bmi
+button.addEventListener('click', () => {
+  bmi = mass.value / height.value ** 2
+message.innerHTML = (`your bmi is ${bmi.toFixed(2)}`)
+
+})
