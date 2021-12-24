6ecd98a601b12ce20b8064431b9a97a958f3d7b4
ilham <ibouaik@gmail.com>
2021-12-24 23:04:30 +0100

    Nember Generator

--- js/day12.js
+++ js/day12.js
@@ -1,28 +1,40 @@
-const button = document.getElementById('button')
-const input = document.getElementById("input")
-const message = document.getElementById("message")
+const input = document.getElementById('input');
+const button = document.getElementById('butt');
+const container = document.getElementById("container");
 
+const isPrime = (num) => {
+    for (let i = 2; i < num; i++) if (num % i === 0) return false;
+    return num > 1;
+  };
 
-button.addEventListener('click', (e) => {
+const Generate = (value) => {
+    for (let i = 0; i <= value; i++) {
+        let number;
+        number = document.createElement("h1");
+        number.className = "number";
+        number.style.fontSize = "30px";
+        number.style.margin = "10px";
+        number.style.padding = "10px";
+        number.style.width = "50px";
+        number.style.textAlign = "center";
+        number.innerHTML = i;
+      
+        if (isPrime(i)) {
+          number.style.backgroundColor = "red";
+        } else if (i % 2 === 0) {
+          number.style.backgroundColor = "yellow";
+        } else {
+          number.style.backgroundColor = "green";
+        }
+        container.appendChild(number);
+      } 
+  }
+
+  button.addEventListener('click', () => {
+    container.innerHTML = ""
     let value = input.value
-   
-    if(value %2 === 0) {
-        message.innerHTML = "even"
-    } else {
-        message.innerHTML = "odd"
-    }
+    Generate(value)
+    input.value = ""
+  })
+  
 
-    
-})
-//////////////////////////////////////////////////////////////////////
-const mass = document.querySelector('#mass')
-const height = document.querySelector('#height')
-const button = document.querySelector('button')
-const message = document.getElementById("message")
-
-let bmi
-button.addEventListener('click', () => {
-  bmi = mass.value / height.value ** 2
-message.innerHTML = (`your bmi is ${bmi.toFixed(2)}`)
-
-})
